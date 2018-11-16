f07f1b1c428a1756718ef658a52af067e2aa7d5f
fdmazzone <fdmazzone@gmail.com>
2018-11-16 14:53:53 -0300

    no se que cambio esoy poniendo

--- Teoria_Basica/scripts/coor_canon.py
+++ Teoria_Basica/scripts/coor_canon.py
@@ -1,14 +1,13 @@
->>> from sympy import *
->>> x,y,epsilon=symbols('x,y,epsilon')
->>> T=Matrix([x/(1-epsilon*x),y/(1-epsilon*x)])
->>> xi=T[0].diff(epsilon).subs(epsilon,0)
->>> xi
+x,y,epsilon=symbols('x,y,epsilon')
+T=Matrix([x/(1-epsilon*x),y/(1-epsilon*x)])
+xi=T[0].diff(epsilon).subs(epsilon,0)
+xi
 x**2
->>> eta=T[1].diff(epsilon).subs(epsilon,0)
->>> eta/xi
+eta=T[1].diff(epsilon).subs(epsilon,0)
+eta/xi
 y/x
->>> y=Function('y')(x)
->>> dsolve(y.diff(x)-y/x,y)
+y=Function('y')(x)
+dsolve(y.diff(x)-y/x,y)
 y(x) == C1*x
->>> Integral(1/xi,x).doit()
+Integral(1/xi,x).doit()
 -1/x
